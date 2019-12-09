b7597716e6c4062ff9fc60b1620227eddc47bbe6
student <anna_stepchenko89@mail.ru>
2019-12-09 17:06:30 +0300

    матрицы

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+public static void main (String [] args) {
+
+    int[][] matrix = new int[10][10];
+
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix.length; j++) {
+            matrix[i][j] = i * j;
+        }
     }
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix.length; j++) {
+            System.out.print(matrix[i][j] + " ");
+        }
+        System.out.print();
+    }
+
 }
+
