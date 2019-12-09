a814c0c2b58af10c722b069d706c3d0ca33c85af
student <anna_stepchenko89@mail.ru>
2019-12-09 17:29:12 +0300

    if / else

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
+
 public class Main {
+    public static void main(String[] args) {
 
-public static void main (String [] args) {
+        int x = new Random().nextInt(10);
 
-    int[][] matrix = new int[10][10];
-
-    for (int i = 0; i < matrix.length; i++) {
-        for (int j = 0; j < matrix.length; j++) {
-            matrix[i][j] = i * j;
+        if (x > 5) {
+            System.out.print("x > 5");
+        }
+        if (x < 5) {
+            System.out.print("x < 5");
+        }
+        if (x == 10) {
+            System.out.print("x == 10");
+        }
+        if (x >= 5) {
+            System.out.print("x >= 5");
+        }
+        if (x <= 5) {
+            System.out.print("x <= 5");
+        }
+        if (x > 3 && x < 6) {
+            System.out.print("x > 3 && x < 6");
+        } else {
+            System.out.print("AND wrong!");
+        }
+        if (x < 3 || x > 6) {
+            System.out.print("x < 3 || x > 6");
+        } else {
+            System.out.print("OR wrong!");
         }
     }
-    for (int i = 0; i < matrix.length; i++) {
-        for (int j = 0; j < matrix.length; j++) {
-            System.out.print(matrix[i][j] + " ");
-        }
-        System.out.print();
-    }
-
 }
-
