3027077976b15949e069e93e17f1327ae475dd2a
student <anna_stepchenko89@mail.ru>
2019-12-10 18:32:12 +0300

    primer

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,23 @@
+import lesson3.Human;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Random random = new Random();
+        Human[] human = new Human[10];
+        for (int i = 0; i < human.length; i++) {
+            
+            int arg = random.nextInt(50);
 
-        //int x = new Random().nextInt(10);
-
-      //  if (x > 5) {
-      //      System.out.print("x > 5");
-     //   }
-     //   if (x < 5) {
-     //       System.out.print("x < 5");
-     //   }
-      //  if (x == 10) {
-     //       System.out.print("x == 10");
-     //   }
-     //   if (x >= 5) {
-     //       System.out.print("x >= 5");
-     //   }
-     //   if (x <= 5) {
-     //       System.out.print("x <= 5");
-     //   }
-     //   if (x > 3 && x < 6) {
-     //       System.out.print("x > 3 && x < 6");
-     //   } else {
-      //      System.out.print("AND wrong!");
-      //  }
-      //  if (x < 3 || x > 6) {
-     //       System.out.print("x < 3 || x > 6");
-     //   } else {
-     //       System.out.print("OR wrong!");
-     //   }
-      //  Scanner scanner = new Scanner(System.in);
-     //   System.out.println("Ввведите первое число:");
-     //   int a = scanner.nextInt();
-      //  System.out.println("Ввведите второе число:");
-      //  int b = scanner.nextInt();
-      //  int sum = a + b;
-      //  System.out.println("Сумма чисел" + sum);
-
-
-         // Scanner scanner = new Scanner(System.in);
-        //String newlogin = "testlog";
-        //String newpassword = "testpasword";
-          //  System.out.println("Ввведите логин:");
-           // String login = scanner.nextLine();
-            //if (login.equals())
-            //System.out.println("Ввведите пароль:");
-            //String password = scanner.nextLine();
-
-            //String newlogin = "testlog";
-            ///String newpassword = "testpasword";
-
-
-       //if ;
-      //      System.out.println("Сумма чисел"+sum);
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Ввведите число:");
-        int newarr = scanner.nextInt();
-        int newarrr = scanner.nextInt();
-        int [] [] arr = new int [newarr][newarrr];
-        for (int i=1; i < arr.length; i ++){
-            for (int j = 1; j <= arr [i-1].length; j++) {
-                System.out.print(i*j + " ");
+            char[] chars = new char[50];
+            for (int j = 0; j < 50; j++) {
+                chars[i] = (char) random.nextInt(150);
             }
 
-        System.out.println();
+            Human human = new Human(new String (chars), age);
+            human [i] = human;
         }
-
-        Scanner scanner = new Scanner(System.in);
-        String newlogin = "testlog";
-        String newpassword = "testpasword";
-        System.out.println("Ввведите логин:");
-        String newlogin = scanner.nextLine();
-        if (login.equals())
-        //System.out.println("Ввведите пароль:");
-        //String password = scanner.nextLine();
-
-        //String newlogin = "testlog";
-        ///String newpassword = "testpasword";
-
-
-        //if ;
-        //      System.out.println("Сумма чисел"+sum);
-}
     }
+}
---
+++ src/lesson3/Human.java
@@ -0,0 +1,21 @@
+package lesson3;
+
+public class Human {
+        //public String height;
+        public int age;
+        //public int weight;
+        public String name;
+
+        public Human (int age, String name) {
+            //
+            //this.name = name;
+            //this.age = age;
+            //this.weight = weight;
+        }
+
+        public Human (){}
+
+       public String getName() {
+            return name;
+       }
+}
